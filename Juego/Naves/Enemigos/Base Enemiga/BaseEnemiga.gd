b240158a9d30afd71027e9061478e4667dfda6b0
Pappermind <agurubisalce@gmail.com>
2023-11-14 23:58:52 -0300

    Finalizando Enemigo Orbital y base Enemiga

--- Juego/Naves/Enemigos/Base Enemiga/BaseEnemiga.gd
+++ Juego/Naves/Enemigos/Base Enemiga/BaseEnemiga.gd
@@ -5,16 +5,22 @@ extends Node2D
 ##Atributos Export
 export var hitpoints:float = 30.0
 export var orbital:PackedScene = null
+export var numero_orbitales: int = 10
+export var intervalo_spawn : float = 0.8
 
 ##Atributos Onready
 onready var impacto_sfx: AudioStreamPlayer2D = $ImpactoSFX
+onready var timer_spawner: Timer = $TimerSpawnerEnemigos
 
 ##Atributos
 var esta_destruida:bool = false
+var posicion_spawn:Vector2 = Vector2.ZERO
 
 ##Metodos
 func _ready() -> void:
+	timer_spawner.wait_time = intervalo_spawn
 	$AnimationPlayer.play(elegir_animacion_aleatoria())
+
 
 ##Metodos Customs
 func elegir_animacion_aleatoria() -> String:
@@ -35,12 +41,14 @@ func recibir_danio(danio: float) -> void:
 	impacto_sfx.play()
 
 func spawnear_orbital() -> void:
-	var pos_spawn: Vector2 = deteccion_cuadrante()
+	numero_orbitales -= 1
+	$RutaEnemigo.global_position = global_position
 	
 	var new_orbital: EnemigoOrbital = orbital.instance()
 	new_orbital.crear(
-		global_position + pos_spawn,
-		self
+		global_position + posicion_spawn,
+		self,
+		$RutaEnemigo
 	)
 	Eventos.emit_signal("spawn_orbital", new_orbital)
 
@@ -55,17 +63,21 @@ func deteccion_cuadrante() -> Vector2:
 	
 	if abs(angulo_player) <= 45.0:
 		#Player entra por la derecha
+		$RutaEnemigo.rotation_degrees = 180.0
 		return $PosicionesSpawn/Este.position
 	elif abs(angulo_player) > 135.0 and abs(angulo_player) <= 180.0:
 		#Player entra por la izquierda
+		$RutaEnemigo.rotation_degrees = 0.0
 		return $PosicionesSpawn/Oeste.position
 	elif abs(angulo_player) > 45.0 and abs(angulo_player) <=135.0:
 		#Player entra por arriba o por abajo
 		if sign(angulo_player) > 0:
 			#Player entra por abajo
+			$RutaEnemigo.rotation_degrees = 270.0
 			return $PosicionesSpawn/Sur.position
 		else:
 			#Player entra por arriba
+			$RutaEnemigo.rotation_degrees = 90
 			return $PosicionesSpawn/Norte.position
 		
 	return $PosicionesSpawn/Norte.position
@@ -90,4 +102,14 @@ func _on_AreaColision_body_entered(body: Node) -> void:
 
 func _on_VisibilityNotifier2D_screen_entered() -> void:
 	$VisibilityNotifier2D.queue_free()
+	posicion_spawn = deteccion_cuadrante()
+	spawnear_orbital()
+	timer_spawner.start()
+
+
+
+func _on_TimerSpawnerEnemigos_timeout() -> void:
+	if numero_orbitales == 0:
+		timer_spawner.stop()
+		return
 	spawnear_orbital()
